8ad94b73fbe3b1c3f0ff0879c5c4e09e6180261e
hussain babatunde <tudemine@gmail.com>
2023-01-18 09:48:15 +0100

    throwing error

--- App.js
+++ App.js
@@ -92,62 +92,62 @@ export function TabNavigation() {
 
 export default function App() {
 
-  const requestUserPermission = async ()=>{
-    const authStatus = await messaging().requestPermission();
-  const enabled =
-    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-    authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+//   const requestUserPermission = async ()=>{
+//     const authStatus = await messaging().requestPermission();
+//   const enabled =
+//     authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+//     authStatus === messaging.AuthorizationStatus.PROVISIONAL;
 
-  if (enabled) {
-    console.log('Authorization status:', authStatus);
-  }
-  }
+//   if (enabled) {
+//     console.log('Authorization status:', authStatus);
+//   }
+//   }
 
-  useEffect(()=>{
-    if (requestUserPermission()){
-      //return fcm token for the device
-      messaging().getToken().then(token => {
-        console.log(token)
-      })
-    }
-    else{
-      console.log("Failed token status", authStatus)
-    }
+//   useEffect(()=>{
+//     if (requestUserPermission()){
+//       //return fcm token for the device
+//       messaging().getToken().then(token => {
+//         console.log(token)
+//       })
+//     }
+//     else{
+//       console.log("Failed token status", authStatus)
+//     }
 
-    // Check whether an initial notification is available
-    messaging()
-      .getInitialNotification()
-      .then(async (remoteMessage) => {
-        if (remoteMessage) {
-          console.log(
-            'Notification caused app to open from quit state:',
-            remoteMessage.notification,
-          );
-        }
-      });
+//     // Check whether an initial notification is available
+//     messaging()
+//       .getInitialNotification()
+//       .then(async (remoteMessage) => {
+//         if (remoteMessage) {
+//           console.log(
+//             'Notification caused app to open from quit state:',
+//             remoteMessage.notification,
+//           );
+//         }
+//       });
 
-      //when the app is running, but in the background
-      // Assume a message-notification contains a "type" property in the data payload of the screen to open
+//       //when the app is running, but in the background
+//       // Assume a message-notification contains a "type" property in the data payload of the screen to open
 
-    messaging().onNotificationOpenedApp(async (remoteMessage) => {
-      console.log(
-        'Notification caused app to open from background state:',
-        remoteMessage.notification,
-      );
-    });
+//     messaging().onNotificationOpenedApp(async (remoteMessage) => {
+//       console.log(
+//         'Notification caused app to open from background state:',
+//         remoteMessage.notification,
+//       );
+//     });
 
-    // Register background handler
-    messaging().setBackgroundMessageHandler(async remoteMessage => {
-      console.log('Message handled in the background!', remoteMessage);
-    });
+//     // Register background handler
+//     messaging().setBackgroundMessageHandler(async remoteMessage => {
+//       console.log('Message handled in the background!', remoteMessage);
+//     });
 
-    const unsubscribe = messaging().onMessage(async remoteMessage => {
-      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
-    });
+//     const unsubscribe = messaging().onMessage(async remoteMessage => {
+//       Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+//     });
 
-return unsubscribe;
+// return unsubscribe;
 
-  }, [])
+//   }, [])
 
 
   let user = false;
