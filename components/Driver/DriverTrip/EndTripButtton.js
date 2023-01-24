6e70e7cb5d1db848be619f4d9dc4cb9568145f26
ndukwesamuel <ndukwesamuel23@yahoo.com>
2023-01-24 14:15:22 +0100

    new

--- components/Driver/DriverTrip/EndTripButtton.js
+++ components/Driver/DriverTrip/EndTripButtton.js
@@ -1,13 +1,99 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import React from "react";
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useEffect, useState } from "react";
 import { Card } from "react-native-shadow-cards";
 
+import NetInfo from "@react-native-community/netinfo";
+import Geolocation from "react-native-geolocation-service";
+
+// const NetworkState = NetInfo.addEventListener((state) => {
+//   console.log("Connection type", state.type);
+//   console.log("Is connected?", state.isConnected);
+//   console.log("Is isInternetReachable?", state.isInternetReachable);
+
+//   if (state.isConnected == false) {
+//     Alert.alert("Alert", "No Internet Connection", [{ text: "OK" }], {
+//       cancelable: false,
+//     });
+//     return false;
+//   }
+
+//   if (state.isInternetReachable == false) {
+//     Alert.alert(
+//       "Alert",
+//       "Internet Connection not Accessible",
+//       [{ text: "OK" }],
+//       { cancelable: false }
+//     );
+//     return false;
+//   }
+// });
+
+// NetworkState = async () => {
+//     let state = await NetInfo.fetch();
+//     if(state.isConnected == false)
+//     {
+//         Alert.alert(
+//             'Alert',
+//             'No Internet Connection',
+//             [
+//               {text: 'OK'},
+//             ],
+//             {cancelable: false},
+//           );
+//           return false;
+//     }
+//     if(state.isInternetReachable == false)
+//     {
+//         Alert.alert(
+//             'Alert',
+//             'Internet Connection not Accessible',
+//             [
+//               {text: 'OK'},
+//             ],
+//             {cancelable: false},
+//           );
+//           return false;
+//     }
+// }
+
 const EndTripButtton = () => {
-  const stopTrip = () => {
-    console.log("working ");
+  const [isConnected, setIsConnected] = useState(true);
+  const [isInternetReachable, setIsInternetReachable] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener((state) => {
+      setIsConnected(state.isConnected);
+      setIsInternetReachable(state.isInternetReachable);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const NetworkState = () => {
+    if (isConnected == false) {
+      Alert.alert("Alert", "No Internet Connection", [{ text: "OK" }], {
+        cancelable: false,
+      });
+      return false;
+    }
+    if (isInternetReachable == false) {
+      Alert.alert(
+        "Alert",
+        "Internet Connection not Accessible",
+        [{ text: "OK" }],
+        { cancelable: false }
+      );
+      return false;
+    }
   };
+
+  const stopTrip = () => {
+    NetworkState();
+    if ((isConnected = false)) return false;
+  };
+
   return (
-    <View className="  ">
+    <View className="">
       <Card className="p-5 w-full ">
         <TouchableOpacity
           onPress={stopTrip}
