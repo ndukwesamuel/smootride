7f4ae53495bd32e4c077db499fb583a8325fd01a
ndukwesamuel <ndukwesamuel23@yahoo.com>
2023-02-17 11:59:41 +0100

    fixed the trip route

--- components/rider/TripTimeline.js
+++ components/rider/TripTimeline.js
@@ -1,29 +1,30 @@
-import React from 'react';
-import Timeline from 'react-native-timeline-flatlist';
+import React from "react";
+import Timeline from "react-native-timeline-flatlist";
 
+const Triptimeline = ({ wayPoints }) => {
+  // console.log({ wayPoints });
 
-const Triptimeline= ({wayPoints}) =>{
-    return (
-        <Timeline
-            innerCircle={"dot"}
-            lineColor="#005091"
-            circleColor="#007cc2"
-            timeContainerStyle={{ minWidth: 52, marginTop: 0 }}
-            descriptionStyle={{ color: "gray" }}
-            data={wayPoints}
-            titleStyle={{ color: "gray", fontSize: 14 }}
-            timeStyle={{
-              textAlign: "center",
-              backgroundColor: "#005091",
-              color: "white",
-              padding: 2,
-              borderRadius: 13,
-            }}
-            options={{
-              style: { marginTop: 5 },
-            }}
-          />
-    )
-}
+  return (
+    <Timeline
+      innerCircle={"dot"}
+      lineColor="#005091"
+      circleColor="#007cc2"
+      timeContainerStyle={{ minWidth: 52, marginTop: 0 }}
+      descriptionStyle={{ color: "gray" }}
+      data={wayPoints}
+      titleStyle={{ color: "gray", fontSize: 14 }}
+      timeStyle={{
+        textAlign: "center",
+        backgroundColor: "#005091",
+        color: "white",
+        padding: 2,
+        borderRadius: 13,
+      }}
+      options={{
+        style: { marginTop: 5 },
+      }}
+    />
+  );
+};
 
 export default Triptimeline;
